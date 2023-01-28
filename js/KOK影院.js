b13001b977b3bcbe929ebe21ba0193989bb8deed
hokamo <86771744+hokamo@users.noreply.github.com>
2023-01-29 00:56:49 +0900

    Add files via upload

---
+++ js/KOK影院.js
@@ -0,0 +1,38 @@
+var rule = {
+    title'KOK影院',
+    host'httpswww.kokyy.com',
+     url'koksfyclasspagefypage.html',
+    url'koksfyclassfyfilter.html',
+    filterable1,是否启用分类筛选,
+    filter_url'{{fl.area}}{{fl.by}}{{fl.class}}pagefypage{{fl.year}}',
+    filter {kokdy[{keyclass,name类型,value[{n全部,v},{n喜剧,vclass喜剧},{n爱情,vclass爱情},{n恐怖,vclass恐怖},{n动作,vclass动作},{n科幻,vclass科幻},{n剧情,vclass剧情},{n战争,vclass战争},{n警匪,vclass警匪},{n犯罪,vclass犯罪},{n动画,vclass动画},{n奇幻,vclass奇幻},{n武侠,vclass武侠},{n冒险,vclass冒险},{n枪战,vclass枪战},{n恐怖,vclass恐怖},{n悬疑,vclass悬疑},{n惊悚,vclass惊悚},{n经典,vclass经典},{n青春,vclass青春},{n文艺,vclass文艺},{n微电影,vclass微电影},{n古装,vclass古装},{n历史,vclass历史},{n运动,vclass运动},{n农村,vclass农村},{n儿童,vclass儿童},{n网络电影,vclass网络电影}]},{keyarea,name地区,value[{n全部,v},{n大陆,varea大陆},{n香港,varea香港},{n台湾,varea台湾},{n美国,varea美国},{n法国,varea法国},{n英国,varea英国},{n日本,varea日本},{n韩国,varea韩国},{n德国,varea德国},{n泰国,varea泰国},{n印度,varea印度},{n意大利,varea意大利},{n西班牙,varea西班牙},{n加拿大,varea加拿大},{n其他,varea其他}]},{keyyear,name年份,value[{n全部,v},{n2023,vyear2023},{n2023,vyear2023},{n2022,vyear2022},{n2021,vyear2021},{n2020,vyear2020},{n2019,vyear2019},{n2018,vyear2018},{n2017,vyear2017},{n2016,vyear2016},{n2015,vyear2015},{n2014,vyear2014},{n2013,vyear2013},{n2012,vyear2012},{n2011,vyear2011},{n2010,vyear2010},{n2009,vyear2009},{n2008,vyear2008},{n2007,vyear2007},{n2006,vyear2006},{n2005,vyear2005},{n2004,vyear2004}]},{keyby,name排序,value[{n全部,v},{n时间,vbytime},{n人气,vbyhits},{n评分,vbyscore}]}],kokds[{keyclass,name类型,value[{n全部,v},{n古装,vclass古装},{n战争,vclass战争},{n青春,vclass青春},{n喜剧,vclass喜剧},{n家庭,vclass家庭},{n犯罪,vclass犯罪},{n动作,vclass动作},{n奇幻,vclass奇幻},{n剧情,vclass剧情},{n历史,vclass历史},{n经典,vclass经典},{n乡村,vclass乡村},{n情景,vclass情景},{n商战,vclass商战},{n网剧,vclass网剧},{n其他,vclass其他}]},{keyarea,name地区,value[{n全部,v},{n大陆,varea大陆},{n韩国,varea韩国},{n香港,varea香港},{n台湾,varea台湾},{n日本,varea日本},{n美国,varea美国},{n泰国,varea泰国},{n英国,varea英国},{n新加坡,varea新加坡},{n其他,varea其他}]},{keyyear,name年份,value[{n全部,v},{n2023,vyear2023},{n2023,vyear2023},{n2022,vyear2022},{n2021,vyear2021},{n2020,vyear2020},{n2019,vyear2019},{n2018,vyear2018},{n2017,vyear2017},{n2016,vyear2016},{n2015,vyear2015},{n2014,vyear2014},{n2013,vyear2013},{n2012,vyear2012},{n2011,vyear2011},{n2010,vyear2010},{n2009,vyear2009},{n2008,vyear2008},{n2007,vyear2007},{n2006,vyear2006},{n2005,vyear2005},{n20042023,vyear20042023}]},{keyby,name排序,value[{n全部,v},{n时间,vbytime},{n人气,vbyhits},{n评分,vbyscore}]}],kokzy[{keyclass,name类型,value[{n全部,v},{n选 秀,vclass选秀},{n情感,vclass情感},{n访谈,vclass访谈},{n播报,vclass播报},{n旅游,vclass旅游},{n音乐,vclass音乐},{n美食,vclass美食},{n纪实,vclass纪实},{n曲艺,vclass曲艺},{n生活,vclass生活},{n游戏互动,vclass游戏互动},{n财经,vclass财经},{n求职,vclass求职}]},{keyarea,name地区,value[{n全部,v},{n大陆,varea大陆},{n韩国,varea韩国},{n香港,varea香港},{n日本,varea日本},{n美国,varea美国},{n其他,varea其他}]},{keyyear,name年份,value[{n全部,v},{n2023,vyear2023},{n2023,vyear2023},{n2022,vyear2022},{n2021,vyear2021},{n2020,vyear2020},{n2019,vyear2019},{n2018,vyear2018},{n2017,vyear2017},{n2016,vyear2016},{n2015,vyear2015},{n2014,vyear2014},{n2013,vyear2013},{n2012,vyear2012},{n2011,vyear2011},{n2010,vyear2010}]},{keyby,name排序,value[{n全部,v},{n时间,vbytime},{n人气,vbyhits},{n评分,vbyscore}]}],kokdm[{keyclass,name类型,value[{n全部,v},{n情感,vclass情感},{n科幻,vclass科幻},{n热血,vclass热血},{n推理,vclass推理},{n搞笑,vclass搞笑},{n冒险,vclass冒险},{n萝莉,vclass萝莉},{n校园,vclass校园},{n动作,vclass动作},{n机战,vclass机战},{n运动,vclass运动},{n战争,vclass战争},{n少年,vclass少年},{n少女,vclass少女},{n社会,vclass社会},{n原创,vclass原创},{n亲子,vclass亲子},{n益智,vclass益智},{n励志,vclass励志},{n其他,vclass其他}]},{keyarea,name地区,value[{n全部,v},{n大陆,varea大陆},{n日本,varea日本},{n欧美,varea欧美},{n其他,varea其他}]},{keyyear,name年份,value[{n全部,v},{n2023,vyear2023},{n2022,vyear2022},{n2021,vyear2021},{n2020,vyear2020},{n2019,vyear2019},{n2018,vyear2018},{n2017,vyear2017},{n2016,vyear2016},{n2015,vyear2015},{n2014,vyear2014},{n2013,vyear2013},{n2012,vyear2012},{n2011,vyear2011},{n2010,vyear2010}]},{keyby,name排序,value[{n全部,v},{n时间,vbytime},{n人气,vbyhits},{n评分,vbyscore}]}],kokjl[{keyclass,name类型,value[{n全部,v},{n社会,vclass社会},{n动物,vclass动物},{n文化,vclass文化},{n自然,vclass自然},{n人文,vclass人文},{n军事,vclass军事},{n历史,vclass历史},{n记录,vclass记录}]},{keyarea,name地区,value[{n全部,v},{n大陆,varea大陆},{n日本,varea日本},{n美国,varea美国},{n国外,varea国外},{n其他,varea其他}]},{keyyear,name年份,value[{n全部,v},{n2023,vyear2023},{n2023,vyear2023},{n2022,vyear2022},{n2021,vyear2021},{n2020,vyear2020},{n2019,vyear2019},{n2018,vyear2018},{n2017,vyear2017},{n2016,vyear2016},{n2015,vyear2015},{n2014,vyear2014},{n2013,vyear2013},{n2012,vyear2012},{n2011,vyear2011},{n2010,vyear2010}]},{keyby,name排序,value[{n全部,v},{n时间,vbytime},{n人气,vbyhits},{n评分,vbyscore}]}]},
+	filter_def{
+		kokdy{by'bytime'},
+		kokds{by'bytime'},
+		kokzy{by'bytime'},
+		kokdm{by'bytime'},
+		kokjl{by'bytime'}
+	},
+    searchable2,是否启用全局搜索,
+    quickSearch0,是否启用快速搜索,
+    headers{
+        'User-Agent''MOBILE_UA'
+    },
+    class_parse '.home-nav&&.nav;a&&Text;a&&href;.(.).html',
+    play_parsetrue,
+    lazyjsvar html=JSON.parse(request(input).match(r player_.=(.))[1]);log(html);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(m3u8mp4.test(url)){input=url}else{input},
+    limit6,
+    推荐'',
+    一级'.movie-list-body&&.movie-list-item;.movie-title&&Text;.movie-post-lazyload&&data-original;.movie-rating&&Text;a&&href',
+    二级{
+        titleh1&&Text;.scroll-content&&Text,
+        img.poster&&img&&src,
+        desc.cr3eq(0)&&Text;;;.cr3.starLink&&Text;,
+        content.detailsTxt&&Text,
+        tabs.swiper-wrapper&&a,
+        lists.content_playlisteq(#id)&&li
+    },
+    searchUrl'index.phpajaxsuggestmid=1&wd=',
+	detailUrl'kokdfyid.html', 非必填,二级详情拼接链接
+	搜索'jsonlist;name;pic;;id',
+}
